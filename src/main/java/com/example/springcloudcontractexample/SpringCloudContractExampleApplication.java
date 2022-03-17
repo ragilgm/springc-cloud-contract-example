f01ff0e7bb8834352eadbc3a563e00a60e4a9480
Ragil Gilang Maulana <ragil.maulana-consultant@prudential.co.id>
2022-03-17 08:08:11 +0700

    first commit

---
+++ src/main/java/com/example/springcloudcontractexample/SpringCloudContractExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudcontractexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudContractExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudContractExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcloudcontractexample/SpringCloudContractExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudcontractexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudContractExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
